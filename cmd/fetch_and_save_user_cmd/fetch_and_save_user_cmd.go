b9db14746e9bfe9a2bc683eebb5dd14d3fbd5db9
Taufan Raifaldy Maaruf <ext.taufan.maaruf@grabtaxi.com>
2023-08-22 03:44:57 +0700

    refactor: remove unused method

--- cmd/fetch_and_save_user_cmd/fetch_and_save_user_cmd.go
+++ cmd/fetch_and_save_user_cmd/fetch_and_save_user_cmd.go
@@ -1,16 +1,13 @@
 package fetch_and_save_user_cmd
 
 import (
-	"jagaat-technical-task/config"
 	"jagaat-technical-task/csv"
-	"jagaat-technical-task/dto"
 	"jagaat-technical-task/fetch"
 )
 
 //go:generate mockery --name IFetchAndSaveUserLogic --inpackage --case=underscore
 type IFetchAndSaveUserLogic interface {
 	FetchAndSaveUser() error
-	fetchUserDataFromURLArr(cfg config.Config) []dto.User
 }
 
 type FetchAndSaveUserLogicImpl struct {
